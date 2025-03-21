867e7722b42b5a92f1cbf790bb6be86f527b9586
Helena-Mansour <168466850+Helena-Mansour@users.noreply.github.com>
2025-03-21 15:33:57 -0300

    Update Evento.java

--- insee/src/main/java/br/edu/insper/desagil/aps5/insee/Evento.java
+++ insee/src/main/java/br/edu/insper/desagil/aps5/insee/Evento.java
@@ -1,16 +1,1 @@
-package br.edu.insper.desagil.aps5.insee;
 
-
-public class Evento {
-    private final DataTempo inicio;
-    private final DataTempo fim;
-
-    public Evento(DataTempo inicio, DataTempo fim) {
-        this.inicio = inicio;
-        this.fim = fim;
-    }
-
-    public boolean valido() {
-        return inicio.comoInteiro() < fim.comoInteiro();
-    }
-}
